e2b0b4cdaa36a4748d4d356b616d2a525e0604c9
alielabridi <elabridiali@outlook.com>
2016-11-23 21:29:15 +0000

    factored all components

---
+++ AddSubMat.h
@@ -0,0 +1,64 @@
+#ifndef STRASSEN_ADDSUBMAT_H
+#define STRASSEN_ADDSUBMAT_H
+#include "ValueMsg.h"
+
+class addition :public CBase_addition{
+    public:
+    addition(CkMigrateMessage *m) {};
+    addition(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
+    }
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        //if(VERBOSE)CkPrintf("addition run 1:\n");
+        /*wrap the resulting addition in a message of size and send it back to future*/
+        ValueMsg *m = new ValueMsg(size);
+
+
+
+        //if(VERBOSE)CkPrintf("addition run 1-1:\n");
+
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                m->v[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        //if(VERBOSE)CkPrintf("addition run 2:\n");
+
+
+
+        //if(VERBOSE)CkPrintf("addition run 4:\n");
+
+        CkSendToFuture(f, m);
+    }
+
+
+};
+
+class substraction :public CBase_substraction{
+    public:
+    substraction(CkMigrateMessage *m) {};
+    substraction(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
+    }
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        //std::vector<std::vector<int>> C;
+        ValueMsg *m = new ValueMsg(size);
+
+
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                m->v[i][j] = A[i][j] - B[i][j];
+            }
+        }
+        /*wrap the resulting substraction in a message of size and send it back to future*/
+        CkSendToFuture(f, m);
+    }
+};
+
+
+
+#endif 
--- test.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-void main()
-{
-    cout << "Hello, world, from Visual C++!" << endl;
-}
