6a4fdbfe02108c76325cf8ad3467313a71126c64
alielabridi <elabridiali@outlook.com>
2016-11-23 21:29:30 +0000

    Revert "factored all components"
    This reverts commit d1a676285bbff5586c581a2aabff59610a293b0c.

--- strassen.C
+++ strassen.C
@@ -1,11 +1,14 @@
 #include "strassen.decl.h"
-#include "StrassenSub.h"
-#include "AddSubMat.h"
-#include "ValueMsg.h"
-
 int THRESHOLD;
 int VERBOSE;
-
+class ValueMsg : public CMessage_ValueMsg {
+public:
+    std::vector<std::vector<int> > v;
+    //i found a solution to allocate a non fixed sized without segment fault
+    ValueMsg(int size):v(size, std::vector<int>(size)){}
+    //for the time being i am going to stick to array as vectors produce segment fault
+    //int v[4][4];
+ };
 class Main : public CBase_Main {
     public: 
         Main(CkMigrateMessage *m) {};
@@ -71,8 +74,220 @@ class Main : public CBase_Main {
     }  
 };
 
+class addition :public CBase_addition{
+    public:
+    addition(CkMigrateMessage *m) {};
+    addition(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
+    }
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        //if(VERBOSE)CkPrintf("addition run 1:\n");
+        /*wrap the resulting addition in a message of size and send it back to future*/
+        ValueMsg *m = new ValueMsg(size);
 
 
+
+        //if(VERBOSE)CkPrintf("addition run 1-1:\n");
+
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                m->v[i][j] = A[i][j] + B[i][j];
+            }
+        }
+        //if(VERBOSE)CkPrintf("addition run 2:\n");
+
+
+
+        //if(VERBOSE)CkPrintf("addition run 4:\n");
+
+        CkSendToFuture(f, m);
+    }
+
+
+};
+
+class substraction :public CBase_substraction{
+    public:
+    substraction(CkMigrateMessage *m) {};
+    substraction(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
+    }
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        //std::vector<std::vector<int>> C;
+        ValueMsg *m = new ValueMsg(size);
+
+
+        for (int i = 0; i < size; ++i)
+        {
+            for (int j = 0; j < size; ++j)
+            {
+                m->v[i][j] = A[i][j] - B[i][j];
+            }
+        }
+        /*wrap the resulting substraction in a message of size and send it back to future*/
+        CkSendToFuture(f, m);
+    }
+};
+
+class strassenSub: public CBase_strassenSub{
+public:
+    strassenSub(CkMigrateMessage *m) {};
+    //overload the function to take three matrices or four
+    //how should I adapt the class to implement for substraction
+    /*four additions*/
+    strassenSub(CkFuture f,std::vector<std::vector<int>> A,
+                 std::vector<std::vector<int>> B, std::vector<std::vector<int>> C, std::vector<std::vector<int>> D,
+                  int size, char partition){ thisProxy.run(f,A,B,C,D,size,partition); }
+
+
+    void run(CkFuture f,std::vector<std::vector<int>> A,
+                 std::vector<std::vector<int>> B, std::vector<std::vector<int>> C, std::vector<std::vector<int>> D,
+                  int size, char partition){
+    //do the addition/substraction, and wait for for the result then call on it the bigger strassen chare
+    //if needed a big multiplication
+    //inside the bigger strassen chare it will decide whether to use the naive or strassen algo
+    if(partition == '1'){
+        //if(VERBOSE)CkPrintf("here stressen SUB run 1:\n");
+
+        /*first partition M1 = (A11+A22)(B11+B22)*/
+        CkFuture p1add1 = CkCreateFuture(); //A11+A22
+        CkFuture p1add2 = CkCreateFuture(); //B11+B22
+        //i could spawn all the additions needed here   
+        //if(VERBOSE)CkPrintf("here stressen SUB run 2:\n");
+
+        CProxy_addition::ckNew(p1add1,A,B,size);//param not matching yet just a pseudo code
+        CProxy_addition::ckNew(p1add2,C,D,size);//param not matching yet just a pseudo code
+        //if(VERBOSE)CkPrintf("here stressen SUB run 3:\n");
+
+        //ValueMsg *m1 = new ValueMsg(size);
+        //ValueMsg *m2 = new ValueMsg(size);
+        ValueMsg * m1 = (ValueMsg *) CkWaitFuture(p1add1);
+        ValueMsg * m2 = (ValueMsg *) CkWaitFuture(p1add2);
+
+      /*  if(VERBOSE)CkPrintf("value of a11+a22:\n");
+
+        for(int i=0; i<newSize;i++){
+            for (int j = 0; j < newSize; ++j)
+            {
+                if(VERBOSE)CkPrintf("%d ",a11->v[i][j]);
+            }
+                if(VERBOSE)CkPrintf("\n");
+        }*/
+        //if(VERBOSE)CkPrintf("here stressen SUB run 4:\n");
+
+        CkFuture p1 = CkCreateFuture();
+        //if(VERBOSE)CkPrintf("here stressen SUB run 5:\n");
+
+        CProxy_strassen::ckNew(p1,m1->v,m2->v,size); //to do (A11+A22)*(B11+B22) by giving m1->v and m2->v
+        //if(VERBOSE)CkPrintf("here stressen SUB run 6:\n");
+
+        //i could free m1 and m2 at this point
+        //if(VERBOSE)CkPrintf("here stressen SUB run 7:\n");
+
+        //ValueMsg *m3 = new ValueMsg(size);
+        delete m1;
+        delete m2;
+        ValueMsg * m3 = (ValueMsg *) CkWaitFuture(p1);
+        //if(VERBOSE)CkPrintf("here stressen SUB run 8:\n");
+
+        CkSendToFuture(f, m3);
+        //p1 = m3->v; //returned product of the two summation
+        //i can free m3 at this point
+        // thinking of using CkProbeFuture on (p1,p2,p3...) instead of wait to not block the next products
+        //or should I use a block instead? UPDATES: I guess by now that I am using strassenSub It will be ok   
+    }    
+
+    else if(partition == '6' || partition == '7'){
+        /*partition M6 = (A21-A11)(B11+B12)*/
+        /*OR partition M7 = (A12-A22)(B21+B22)*/
+        CkFuture psub1 = CkCreateFuture(); 
+        CkFuture padd2 = CkCreateFuture(); 
+        //i could spawn all the additions needed here   
+        CProxy_substraction::ckNew(psub1,A,B,size);//param not matching yet just a pseudo code
+        CProxy_addition::ckNew(padd2,C,D,size);//param not matching yet just a pseudo code
+        //ValueMsg *m1 = new ValueMsg(size);
+        //ValueMsg *m2 = new ValueMsg(size);
+        ValueMsg * m1 = (ValueMsg *) CkWaitFuture(psub1);
+        ValueMsg * m2 = (ValueMsg *) CkWaitFuture(padd2);
+        CkFuture p = CkCreateFuture();
+        CProxy_strassen::ckNew(p,m1->v,m2->v, size); //to do (A11+A22)*(B11+B22) by giving m1->v and m2->v
+        //i could free m1 and m2 at this point
+        delete m1;
+        delete m2;
+        //ValueMsg *m3 = new ValueMsg(size);
+        ValueMsg * m3 = (ValueMsg *) CkWaitFuture(p);
+        CkSendToFuture(f, m3);
+        //p1 = m3->v; //returned product of the two summation
+        //i can free m3 at this point
+        // thinking of using CkProbeFuture on (p1,p2,p3...) instead of wait to not block the next products
+        //or should I use a block instead? UPDATES: I guess by now that I am using strassenSub It will be ok   
+    }  
+
+
+    }
+
+
+     /*three addition*/
+    strassenSub(CkFuture f,std::vector<std::vector<int>> A,
+             std::vector<std::vector<int>> B, std::vector<std::vector<int>> C,
+              int size, char partition){ thisProxy.run(f,A,B,C,size,partition); }
+
+
+    void run(CkFuture f,std::vector<std::vector<int>> A,
+                 std::vector<std::vector<int>> B, std::vector<std::vector<int>> C,
+                  int size,char partition){
+
+    //do the addition/substraction, and wait for for the result then call on it the bigger strassen chare
+    //if needed a big multiplication
+    //inside the bigger strassen chare it will decide whether to use the naive or strassen algo
+    if(partition == '2' || partition == '5'){
+        /*partition M2 =(A21+A22)B11*/
+        /*OR partition M5 =(A11+A12)B22*/
+        CkFuture padd1 = CkCreateFuture(); 
+        //i could spawn all the additions needed here   
+        CProxy_addition::ckNew(padd1,A,B,size);//param not matching yet just a pseudo code
+        //ValueMsg *m1 = new ValueMsg(size);
+        ValueMsg * m1 = (ValueMsg *) CkWaitFuture(padd1);
+        CkFuture p = CkCreateFuture();
+
+        CProxy_strassen::ckNew(p,m1->v,C, size); 
+        //i could free m1 and m2 at this point
+        delete m1;
+        //ValueMsg *m3 = new ValueMsg(size);
+        ValueMsg * m3 = (ValueMsg *) CkWaitFuture(p);
+        CkSendToFuture(f, m3);
+        //p1 = m3->v; //returned product of the two summation
+        //i can free m3 at this point
+        // thinking of using CkProbeFuture on (p1,p2,p3...) instead of wait to not block the next products
+        //or should I use a block instead? UPDATES: I guess by now that I am using strassenSub It will be ok   
+    } 
+    else if(partition == '3' || partition == '4'){
+        /*partition M3 = A11(B12-B22)*/
+        /*OR partition M4 =A22(B21-B11)*/
+        CkFuture psub1 = CkCreateFuture(); 
+        //i could spawn all the additions needed here   
+        CProxy_substraction::ckNew(psub1,B,C,size);//param not matching yet just a pseudo code
+        //ValueMsg *m1 = new ValueMsg(size);
+        ValueMsg * m1 = (ValueMsg *) CkWaitFuture(psub1);
+        CkFuture p = CkCreateFuture();
+        CProxy_strassen::ckNew(p,A,m1->v, size); 
+        //i could free m1 and m2 at this point
+        delete m1;
+        //ValueMsg *m3 = new ValueMsg(size);
+        ValueMsg * m3 = (ValueMsg *) CkWaitFuture(p);
+        CkSendToFuture(f, m3);
+        //p1 = m3->v; //returned product of the two summation
+        //i can free m3 at this point
+        // thinking of using CkProbeFuture on (p1,p2,p3...) instead of wait to not block the next products
+        //or should I use a block instead? UPDATES: I guess by now that I am using strassenSub It will be ok   
+    } 
+
+
+    }
+
+};
 class strassen : public CBase_strassen  {
     //private:  int result, count, IamRoot;  CProxy_strassen parent;
 
