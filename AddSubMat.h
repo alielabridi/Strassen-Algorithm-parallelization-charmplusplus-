bc767cc3a384bc48cd9d457ba78197ec4439d798
alielabridi <elabridiali@outlook.com>
2016-11-28 08:19:32 +0000

    Revert "vectorization so far before moving to arrays"
    This reverts commit 8d6dd44663f5cd956a4042d7b0efbe9cb835cdaa.

--- AddSubMat.h
+++ AddSubMat.h
@@ -4,24 +4,35 @@
 
 class addition :public CBase_addition{
     public:
-    addition(CkMigrateMessage *m) {}
-    addition(CkFuture f,AddSubMsg* Msg, int size){
-        thisProxy.run(f,Msg,size);
+    addition(CkMigrateMessage *m) {};
+    addition(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
     }
-    void run(CkFuture f,AddSubMsg* Msg, int size){
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        //if(VERBOSE)CkPrintf("addition run 1:\n");
         /*wrap the resulting addition in a message of size and send it back to future*/
 
   //if(VERBOSE)CkPrintf("Work done by processor %d\n",CkMyPe());
 
+
         ValueMsg *m = new ValueMsg(size);
+
+
+
+        //if(VERBOSE)CkPrintf("addition run 1-1:\n");
 
         for (int i = 0; i < size; ++i)
         {
             for (int j = 0; j < size; ++j)
             {
-                m->v[i][j] = Msg->A[i][j] + Msg->A[i][j];
+                m->v[i][j] = A[i][j] + B[i][j];
             }
         }
+        //if(VERBOSE)CkPrintf("addition run 2:\n");
+
+
+
+        //if(VERBOSE)CkPrintf("addition run 4:\n");
 
         CkSendToFuture(f, m);
     }
@@ -31,11 +42,11 @@ class addition :public CBase_addition{
 
 class substraction :public CBase_substraction{
     public:
-    substraction(CkMigrateMessage *m) {}
-    substraction(CkFuture f,AddSubMsg* Msg, int size){
-        thisProxy.run(f,Msg,size);
+    substraction(CkMigrateMessage *m) {};
+    substraction(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
+        thisProxy.run(f,A,B,size);
     }
-    void run(CkFuture f,AddSubMsg* Msg, int size){
+    void run(CkFuture f,std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, int size){
         //std::vector<std::vector<int>> C;
 
   //if(VERBOSE)CkPrintf("Work done by processor %d\n",CkMyPe());
@@ -47,7 +58,7 @@ class substraction :public CBase_substraction{
         {
             for (int j = 0; j < size; ++j)
             {
-                m->v[i][j] = Msg->A[i][j] - Msg->A[i][j];
+                m->v[i][j] = A[i][j] - B[i][j];
             }
         }
         /*wrap the resulting substraction in a message of size and send it back to future*/
