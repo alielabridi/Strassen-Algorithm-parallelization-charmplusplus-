bbe9e5304806df34525aebb3226d28987bcd445b
alielabridi <elabridiali@outlook.com>
2016-11-28 08:19:32 +0000

    Revert "vectorization so far before moving to arrays"
    This reverts commit 8d6dd44663f5cd956a4042d7b0efbe9cb835cdaa.

--- ValueMsg.h
+++ ValueMsg.h
@@ -1,3 +1,4 @@
+
 #ifndef STRASSEN_VALUEMSG_H
 #define STRASSEN_VALUEMSG_H
 
@@ -6,62 +7,8 @@ public:
     std::vector<std::vector<int> > v;
     //i found a solution to allocate a non fixed sized without segment fault
     ValueMsg(int size):v(size, std::vector<int>(size)){}
-    ValueMsg(){}
-
     //for the time being i am going to stick to array as vectors produce segment fault
     //int v[4][4];
  };
 
-
-class AddSubMsg : public CMessage_AddSubMsg  {
-public:
-    std::vector<std::vector<int> > A;
-    std::vector<std::vector<int> > B;
-    //i found a solution to allocate a non fixed sized without segment fault
-    AddSubMsg(){}
-    AddSubMsg(int size){
-    	A = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    	B = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    }
-
-    //for the time being i am going to stick to array as vectors produce segment fault
-    //int v[4][4];
- };
-
-
-class StrassenMsg : public CMessage_StrassenMsg  {
-public:
-    std::vector<std::vector<int> > A;
-    std::vector<std::vector<int> > B;
-    //i found a solution to allocate a non fixed sized without segment fault
-    StrassenMsg(){}
-    StrassenMsg(int size){
-    	A = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    	B = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    }
-
-    //for the time being i am going to stick to array as vectors produce segment fault
-    //int v[4][4];
- };
-
-
- class StrassenSubMsg : public CMessage_StrassenSubMsg  {
-public:
-    std::vector<std::vector<int> > A;
-    std::vector<std::vector<int> > B;
-    std::vector<std::vector<int> > C;
-    std::vector<std::vector<int> > D;
-    //i found a solution to allocate a non fixed sized without segment fault
-    StrassenSubMsg(){}
-    
-    StrassenSubMsg(int size){
-    	A = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    	B = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    	C = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    	D = std::vector<std::vector<int>> (size,std::vector<int>(size));
-    }
-
-    //for the time being i am going to stick to array as vectors produce segment fault
-    //int v[4][4];
- };
 #endif 
