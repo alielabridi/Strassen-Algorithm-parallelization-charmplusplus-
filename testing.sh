6b659b199a547c657e5893f3d57b886064029dfe
alielabridi <elabridiali@outlook.com>
2016-12-03 06:31:36 +0000

    Revert "correcting typos"
    This reverts commit 12f0bb703a68d239e7dad9122391af3ba68e29c7.

--- testing.sh
+++ testing.sh
@@ -4,6 +4,10 @@
 # do
 # 	for THRESHOLD in {3..6}
 # 		do
+# 			if [$THRESHOLD > $size]
+# 			then
+# 				break
+# 			fi
 # 			for proc in {1..10}
 # 				do
 # 					./charmrun ./strassen $((2**$size)) $((2**$THRESHOLD)) 0 +p$proc
@@ -15,10 +19,33 @@
 # done
 
 
-for size in {10..12}
+# for size in {8..10}
+# do
+# 	for THRESHOLD in {5..7}
+# 		do
+# 			if [$THRESHOLD > $size]
+# 			then
+# 				break
+# 			fi
+# 			for proc in {1..10}
+# 				do
+# 					./charmrun ./strassen $((2**$size)) $((2**$THRESHOLD)) 0 +p$proc
+# 					./OpenMP $((2**$size)) $((2**$THRESHOLD)) $proc
+# 				done
+
+# 		done
+# 	./charmrun ./strassen $((2**$size)) $((2**$size)) 0 +p1
+# done
+
+
+for size in {7..9}
 do
-	for THRESHOLD in {6..7}
+	for THRESHOLD in {5..6}
 		do
+			if [$THRESHOLD > $size]
+			then
+				break
+			fi
 			for proc in {1..10}
 				do
 					./charmrun ./strassen $((2**$size)) $((2**$THRESHOLD)) 0 +p$proc
@@ -28,4 +55,3 @@ do
 		done
 	./charmrun ./strassen $((2**$size)) $((2**$size)) 0 +p1
 done
-
